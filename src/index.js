6846580f3792ccfa9803377f3ad2058d9401f2f5
DinarasN <119352434+DinarasN@users.noreply.github.com>
2023-01-08 14:16:53 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-var num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
-var tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
-
-module.exports = function toReadable(n){
-    if (n < 20) return num[n];
-    var digit = n%10;
-    if (n < 100) return tens[~~(n/10)-2] + (digit? "-" + num[digit]: "");
-    if (n < 1000) return num[~~(n/100)] +" hundred" + (n%100 == 0? "": " " + toReadable(n%100));
-    return toReadable(~~(n/1000)) + " thousand" + (n%1000 != 0? " " + toReadable(n%1000): "");
-}
+module.exports = function toReadable (number) {
+    const first = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+    const tens = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+    const mad = ['', 'thousand', 'million', 'billion', 'trillion'];
+    let word = '';
+  
+    for (let i = 0; i < mad.length; i++) {
+      let tempNumber = number%(100*Math.pow(1000,i));
+      if (Math.floor(tempNumber/Math.pow(1000,i)) !== 0) {
+        if (Math.floor(tempNumber/Math.pow(1000,i)) < 20) {
+          word = first[Math.floor(tempNumber/Math.pow(1000,i))] + mad[i] + ' ' + word;
+        } else {
+          word = tens[Math.floor(tempNumber/(10*Math.pow(1000,i)))] + '-' + first[Math.floor(tempNumber/Math.pow(1000,i))%10] + mad[i] + ' ' + word;
+        }
+      }
+  
+      tempNumber = number%(Math.pow(1000,i+1));
+      if (Math.floor(tempNumber/(100*Math.pow(1000,i))) !== 0) word = first[Math.floor(tempNumber/(100*Math.pow(1000,i)))] + 'hunderd ' + word;
+    }
+      return word;
+  }
